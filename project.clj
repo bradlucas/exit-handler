0ae5fa6d7f216d701dc75abddfa6911b33de51b4
Brad Lucas <brad@beaconhill.com>
2016-10-23 08:46:27 -0400

    version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bradlucas/exit-handler "1.0.0"
+(defproject com.bradlucas/exit-handler "1.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "MIT"
